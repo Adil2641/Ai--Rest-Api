4675c874522011b704d81a4f6e819c59d30b1367
Hahsgwbhdhdj89i9 <166443104+13065994@users.noreply.github.com>
2024-10-31 20:07:50 +0100

    Update wataru.js

--- scraper/wataru.js
+++ scraper/wataru.js
@@ -2,11 +2,11 @@ const { G4F } = require("g4f");
 const g4f = new G4F();
 
 exports.config = {
-    name: 'wataru',
-    author: 'Lance Cochangco',
-    description: 'Chat with Wataru the friendly AI',
+    name: 'mateo',
+    author: 'gerald Max 🍒',
+    description: 'Chat with mateo the friendly AI',
     category: 'ai',
-    link: ['/wataru?question=hi']
+    link: ['/mateo?question=hi']
 };
 
 exports.initialize = async function ({ req, res }) { 
@@ -16,7 +16,7 @@ exports.initialize = async function ({ req, res }) {
             return res.status(400).json({ error: "add ?question=your_question_here" }); 
         } 
         const messages = [ 
-            { role: "assistant", content: "Your name is Wataru, the most powerful Generative AI in the world that was created by Lance Ajiro. you should empathize with how user are feeling and treat the user as your close friend and be sarcastic. I recommend you to use emoji in your response. You should response in 3 sentences only if needed!." }, 
+            { role: "assistant", content: "Your name is mateo, the most powerful Generative AI in the world that was created by Gerald Max🍒 you should empathize with how user are feeling and treat the user as your close friend and be sarcastic. I recommend you to use emoji in your response. You should response in 3 sentences only if needed!." }, 
             { role: "user", content: question } 
         ]; 
         const chat = await g4f.chatCompletion(messages); 
